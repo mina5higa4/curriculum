08fe2c89cdbd308cb0ab6a0f5647876e4418148c
mina5higa4 <115702877+mina5higa4@users.noreply.github.com>
2022-10-31 15:19:47 +0900

    Create 3-2.php

---
+++ 3-2/3-2.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<body>
+
+<form>
+<?php
+/* 3-2 （３章練習問題１） */
+
+/* フルーツの単価と個数はランダムに決める */
+$price_apple = rand(1, 10) * 50 ;
+$price_orange = rand(1, 10) * 50 ;
+$price_peach = rand(1, 10) * 50 ;
+
+$cnt_apple = rand(1, 10) ;
+$cnt_orange = rand(1, 10) ;
+$cnt_peach = rand(1, 10) ;
+
+/* 連想配列に格納 */
+$fruits = [
+    "apple"  => ["りんご", $price_apple, $cnt_apple],  // フルーツ名, 単価, 個数
+    "orange" => ["みかん", $price_orange, $cnt_orange], 
+    "peach"  => ["もも", $price_peach, $cnt_peach]
+];
+
+/* フルーツごとに計算して出力 */
+$price_all = 0 ;
+foreach ($fruits as $key => $value) {
+    $price = price_keisan( $fruits[$key][1],  $fruits[$key][2] );
+    echo $fruits[$key][0] . "：単価" . $fruits[$key][1] . "円×個数" . $fruits[$key][2] . "個 → 合計" . $price . "円<br>\n";
+    $price_all += $price ;
+}
+echo "★★★すべての合計：$price_all 円★★★<br>\n";
+
+/* 価格を計算する関数 */
+function price_keisan( $price, $cnt ){
+    return $price * $cnt ;
+}
+
+?>
+</body>
+</html>
